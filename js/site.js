d38fd84eb0cc2376c928705af4b30892517ee67d
Jamal Gibson <jamal.gibson@rogers.com>
2023-04-21 13:40:16 -0400

    Created mortage calculator logic

--- js/site.js
+++ js/site.js
@@ -5,13 +5,118 @@ const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
 
-function displayMessage() {
-  let msg = document.getElementById("message").value;
-  //alert(msg);
+var USDollar = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
-  Swal.fire({
-    backdrop: false,
-    title: "App Name",
-    text: msg,
-  });
+function calculateAmounts() {
+  let loanAmount = parseFloat(document.getElementById("loanAmount").value);
+  let loanTerm = parseInt(document.getElementById("loanTerm").value);
+  let loanRate = parseFloat(document.getElementById("loanRate").value);
+
+  let monthlyPayment =
+    (loanAmount * (loanRate / 1200)) /
+    (1 - (1 + loanRate / 1200) ** (-1 * loanTerm));
+
+  let totalCost = monthlyPayment * loanTerm;
+  let totalInterest = totalCost - loanAmount;
+
+  setAmounts(monthlyPayment, loanAmount, totalInterest, totalCost);
+  buildTable(loanAmount, loanTerm, loanRate, monthlyPayment);
+}
+
+function setAmounts(monthlyPayment, principalAmount, totalInterest, totalCost) {
+  document.getElementById("monthlyPayment").textContent =
+    USDollar.format(monthlyPayment);
+
+  document.getElementById("totalPrincipal").textContent =
+    USDollar.format(principalAmount);
+
+  document.getElementById("totalCost").textContent = USDollar.format(
+    totalCost
+  );
+
+  document.getElementById("totalInterest").textContent =
+    USDollar.format(totalInterest);
+
+  let paymentStats = {
+    monthlyPayment: monthlyPayment,
+    principalAmount: principalAmount,
+    totalInterest: totalInterest,
+    totalCost: totalCost
+  };
+
+  localStorage.setItem("jgPaymentStats", JSON.stringify(paymentStats));
+}
+
+function buildTable(loanAmount, loanTerm, loanRate, monthlyPayment) {
+  let paymentRows = [
+    {
+      month: 0,
+      payment: 0,
+      principal: 0,
+      interest: 0,
+      totalInterest: 0,
+      balance: loanAmount,
+    },
+  ];
+
+  let paymentTable = document.getElementById("paymentTable")
+  let template = document.getElementById("tableRowTemplate");
+  paymentTable.innerHTML = "";
+
+  for (let i = 1; i <= loanTerm; i++)
+  {
+    let tableRow = document.importNode(template.content, true);
+
+    let interest = paymentRows[i - 1].balance * (loanRate / 1200);
+    let totalInterest = paymentRows[i - 1].totalInterest + interest;
+    let principal = monthlyPayment - interest;
+    let balance = paymentRows[i - 1].balance - principal;
+
+    let paymentRow = {
+      month: i,
+      payment: monthlyPayment,
+      principal: principal,
+      interest: interest,
+      totalInterest: totalInterest,
+      balance: balance
+    }
+
+    paymentRows.push(paymentRow);
+
+    tableRow.querySelector('[data-id="monthNumber"]').textContent = i;
+    tableRow.querySelector('[data-id="payment"]').textContent = USDollar.format(monthlyPayment);
+    tableRow.querySelector('[data-id="principal"]').textContent = USDollar.format(principal);
+    tableRow.querySelector('[data-id="interest"]').textContent = USDollar.format(interest);
+    tableRow.querySelector('[data-id="totalInterest"]').textContent = USDollar.format(totalInterest);
+    tableRow.querySelector('[data-id="balance"]').textContent = USDollar.format(balance);
+
+    paymentTable.appendChild(tableRow);
+  }
+
+  let paymentInput = {
+    loanAmount: loanAmount,
+    loanTerm: loanTerm,
+    loanRate: loanRate,
+    monthlyPayment: monthlyPayment
+  };
+
+
+  localStorage.setItem("jgPaymentInput", JSON.stringify(paymentInput));
+}
+
+function checkData() {
+  data1 = localStorage.getItem("jgPaymentStats");
+  data2 = localStorage.getItem("jgPaymentInput");
+
+  if (data1 != null && data2 != null)
+  {
+    paymentStats = JSON.parse(data1);
+    paymentInput = JSON.parse(data2);
+
+    setAmounts(paymentStats.monthlyPayment, paymentStats.principalAmount, paymentStats.totalInterest, paymentStats.totalCost);
+    buildTable(paymentInput.loanAmount, paymentInput.loanTerm, paymentInput.loanRate, paymentInput.monthlyPayment);
+  }
 }
